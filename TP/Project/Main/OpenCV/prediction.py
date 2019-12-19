4bab02a21e5ac00df7d6d57d0720c94a7e82acff
locky12 <francois.letterle@gmail.com>
2019-12-19 17:37:20 +0100

    refacto predict

--- TP/Project/Main/OpenCV/prediction.py
+++ TP/Project/Main/OpenCV/prediction.py
@@ -46,21 +46,4 @@ class Predict :
 		preds = self.__model.predict(image)
 		i = preds.argmax(axis=1)[0]
 		label = self.__label.classes_[i]
-<<<<<<< HEAD
-=======
-		text = "{}: {:.2f}%".format(label, preds[0][i] * 100)
-		# cv2.putText(output, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
-		# 	(0, 0, 255), 2)
-		# for j in range(5) :
-		# 	print(CLASSE_NAME[j]," : ",preds[0][j])
-		# show the output image
-		# cv2.imshow("Image", output)
-		# cv2.waitKey(0)
-		return CLASSE_NAME[i]
-		# print (tabulate(decode_predictions(preds, top=5)[0], headers=['Name', 'Probability']))
->>>>>>> 6f9f527f4a1c25f837dc2b1568ff55edf63d918e
-
-		# for j in range(5) :
-		# 	print(CLASSE_NAME[j]," : ",preds[0][j])
-
 		return CLASSE_NAME[i]
